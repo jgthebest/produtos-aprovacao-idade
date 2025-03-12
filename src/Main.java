cd9cf92e9b289bc5dc47b23a0a89f3ac4aeda924
jgthebest <jgm.fernandes222@gmail.com>
2025-03-12 10:04:19 -0300

    Alteração final.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Digite sua nota. ");
         int notaDigitada = sc.nextInt();
 
         VerificarAprovacao verificarAprovacao = new VerificarAprovacao(notaDigitada);
